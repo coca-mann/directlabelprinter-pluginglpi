e95f4a123d31b510ea8d06b74f1a3ad7ef9a6a6f
Coca-Mann <juliano.ostroski@gmail.com>
2025-10-26 00:08:38 -0400

    test: api communication

--- setup.php
+++ setup.php
@@ -62,9 +62,10 @@ function plugin_init_directlabelprinter() {
 
     $PLUGIN_HOOKS[Hooks::USE_MASSIVE_ACTION]['directlabelprinter'] = true;
 
-    // $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['directlabelprinter'] = [
-    //     'js/src/directlabelprinter.js' // Caminho relativo à raiz do plugin
-    // ];
+    $relative_build_path = '/public/build/plugins/directlabelprinter/js/directlabelprinter.js';
+    $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['directlabelprinter'] = [
+        $relative_build_path // Caminho relativo à raiz do plugin
+    ];
 
     // You might add other class registrations or hooks here later
 }
